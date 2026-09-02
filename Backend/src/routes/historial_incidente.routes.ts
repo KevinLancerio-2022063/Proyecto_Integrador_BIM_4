1e4b4300c316367c067f3535ba1f2973b11c9313
wdepaz-2025275 <wdepaz-2025275@kinal.edu.gt>
2026-09-02 11:48:27 -0600

    feat: Se modificaron las rutas de la clase historial incidente

--- Backend/src/routes/historial_incidente.routes.ts
+++ Backend/src/routes/historial_incidente.routes.ts
@@ -1,10 +1,22 @@
 import { Router } from "express";
 import { HistorialIncidenteController } from "../controllers/historial_incidente.controller";
+import { IncidenteController } from "../controllers/incidente.controller";
 
 const router = Router();
 
-router.get("/incidente/:incidenteId", HistorialIncidenteController.obtenerPorIncidente);
+// Define la ruta GET para listar todos los incidentes
+router.get("/", HistorialIncidenteController.listarHistorial);
 
-router.post("/", HistorialIncidenteController.crear);
+// Define la ruta GET para buscar un incidente por ID
+router.get("/:id", HistorialIncidenteController.buscarHistorialPorId);
+
+// Define la ruta POST para crear un nuevo incidente
+router.post("/", HistorialIncidenteController.crearHistorial);
+
+// Define la ruta PUT para actualizar un incidente existente
+router.put("/:id", HistorialIncidenteController.actualizarHistorial);
+
+// Define la ruta DELETE para eliminar un incidente
+router.delete("/:id", HistorialIncidenteController.eliminarHistorial);
 
 export default router;
