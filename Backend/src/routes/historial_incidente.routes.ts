7557ac64d5df1412b2bfce0405a67e77812e7bf3
wdepaz-2025275 <wdepaz-2025275@kinal.edu.gt>
2026-09-07 22:22:16 -0600

    fix: Se corrigio el nombre de los controladores de rutas

--- Backend/src/routes/historial_incidente.routes.ts
+++ Backend/src/routes/historial_incidente.routes.ts
@@ -1,22 +1,21 @@
 import { Router } from "express";
 import { HistorialIncidenteController } from "../controllers/historial_incidente.controller";
-import { IncidenteController } from "../controllers/incidente.controller";
 
 const router = Router();
 
 // Define la ruta GET para listar todos los incidentes
-router.get("/", HistorialIncidenteController.listarHistorial);
+router.get("/", HistorialIncidenteController.listarHistorialIncidente);
 
 // Define la ruta GET para buscar un incidente por ID
-router.get("/:id", HistorialIncidenteController.buscarHistorialPorId);
+router.get("/:id", HistorialIncidenteController.buscarHistorialIncidentePorId);
 
 // Define la ruta POST para crear un nuevo incidente
-router.post("/", HistorialIncidenteController.crearHistorial);
+router.post("/", HistorialIncidenteController.crearHistorialIncidente);
 
 // Define la ruta PUT para actualizar un incidente existente
-router.put("/:id", HistorialIncidenteController.actualizarHistorial);
+router.put("/:id", HistorialIncidenteController.actualizarHistorialIncidente);
 
 // Define la ruta DELETE para eliminar un incidente
-router.delete("/:id", HistorialIncidenteController.eliminarHistorial);
+router.delete("/:id", HistorialIncidenteController.eliminarHistorialIncidente);
 
 export default router;
