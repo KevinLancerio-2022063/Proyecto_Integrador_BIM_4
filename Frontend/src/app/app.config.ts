b9d5dc896676a970b60617554f6a5bd234a05371
wdepaz-2025275 <wdepaz-2025275@kinal.edu.gt>
2026-09-09 11:55:23 -0600

    fix: Se modifico la clase app.config del frontend

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,11 +1,13 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { ApplicationConfig, provideZonelessChangeDetection } from "@angular/core";
+import { provideRouter } from "@angular/router";
+import { provideHttpClient } from "@angular/common/http";
+import { routes } from "./app.routes";
 
+// Configura la aplicación en modo zoneless
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration()
+    provideZonelessChangeDetection(),
+    provideRouter(routes),
+    provideHttpClient()
   ]
 };
