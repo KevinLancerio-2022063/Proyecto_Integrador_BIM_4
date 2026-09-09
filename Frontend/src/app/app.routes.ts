2cea5e6fc9125fd9130f7845a155946efea71b1c
wdepaz-2025275 <wdepaz-2025275@kinal.edu.gt>
2026-09-09 11:54:55 -0600

    fix: Se modifico la clase app.routes del frontend

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,26 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { LayoutComponent } from "./features/incidentes/components/layout/layout.component";
 
-export const routes: Routes = [];
+// Definimos las rutas principales con layout
+export const routes: Routes = [
+  {
+    path: "Incidentes",
+    component: LayoutComponent,
+    children: [
+      {
+        path: "incidentes",
+        loadChildren: () => import("./features/incidentes/incidentes.module").then((m) => m.IncidenteModule)
+      },
+      {
+        path: "",
+        redirectTo: "incidentes",
+        pathMatch: "full"
+      }
+    ]
+  },
+  {
+    path: "",
+    redirectTo: "/Incidentes/incidentes",
+    pathMatch: "full"
+  }
+];
