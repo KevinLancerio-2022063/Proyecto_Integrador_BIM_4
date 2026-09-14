52ddc9e5f7005dfd7e3957aa5ddf9294a1e99fcb
wdepaz-2025275 <wdepaz-2025275@kinal.edu.gt>
2026-09-14 11:35:10 -0600

    fix: Se corrigio el nombre e icono de la barra lateral

--- Frontend/src/app/features/incidentes/components/layout/layout.component.ts
+++ Frontend/src/app/features/incidentes/components/layout/layout.component.ts
@@ -15,8 +15,7 @@ import { MatIconModule } from "@angular/material/icon";
 export class LayoutComponent {
   // Define los enlaces del menú lateral
   menuItems = [
-    { label: "Recursos", route: "/logistica/recursos", icon: "inventory_2" },
-    { label: "Refugios", route: "/logistica/refugios", icon: "home" },
-    { label: "Asignaciones", route: "/logistica/asignaciones", icon: "assignment" }
+    { label: "Incidentes", route: "/incidentes", icon: "warning" },
+    { label: "Historial", route: "/historial", icon: "history" },
   ];
 }
