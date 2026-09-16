31fc5471e3e6cd1358bd9b8b7c7da14d705776f4
andersonmucia-2025337 <andersonmucia-2025337@kinal.edu.gt>
2026-09-16 00:53:44 -0600

    feat: se agregó configuración de interceptor de autenticación y Zoneless en app.config

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,11 +1,27 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+    ApplicationConfig,
+    provideBrowserGlobalErrorListeners,
+    provideZonelessChangeDetection
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
+import {
+    provideClientHydration,
+    withEventReplay
+} from '@angular/platform-browser';
+
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { authInterceptor } from './features/core/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration()
-  ]
+    providers: [
+        provideBrowserGlobalErrorListeners(),
+        provideZonelessChangeDetection(),
+        provideRouter(routes),
+        provideClientHydration(withEventReplay()),
+        provideHttpClient(
+            withFetch(),
+            withInterceptors([authInterceptor])
+        )
+    ]
 };
