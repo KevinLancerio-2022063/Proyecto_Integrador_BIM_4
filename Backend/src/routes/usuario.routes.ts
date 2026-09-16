4599cd4f293fa9de941325953ee58317796a45f9
andersonmucia-2025337 <andersonmucia-2025337@kinal.edu.gt>
2026-09-16 00:46:50 -0600

    feat: se agregó el CRUD de usuarios con protección de rutas y roles

--- Backend/src/routes/usuario.routes.ts
+++ Backend/src/routes/usuario.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from 'express';
 import { UsuarioController } from '../controllers/usuario.controller';
 import { validateUsuario } from '../validators/usuario.validator';
+import { authMiddleware, roleMiddleware } from '../middlewares/auth.middleware';
 
 const router = Router();
 const controller = new UsuarioController();
 
-router.get('/', controller.findAll.bind(controller));
-router.get('/:id', controller.findById.bind(controller));
-router.post('/', validateUsuario, controller.create.bind(controller));
-router.put('/:id', validateUsuario, controller.update.bind(controller));
-router.delete('/:id', controller.delete.bind(controller));
+router.get('/', authMiddleware, controller.findAll.bind(controller));
+router.get('/:id', authMiddleware, controller.findById.bind(controller))
+router.post('/', authMiddleware, roleMiddleware('ADMIN'), validateUsuario, controller.create.bind(controller));
+router.put('/:id', authMiddleware, roleMiddleware('ADMIN'), validateUsuario, controller.update.bind(controller));
+router.delete('/:id', authMiddleware, roleMiddleware('ADMIN'), controller.delete.bind(controller));
 
 export default router;
