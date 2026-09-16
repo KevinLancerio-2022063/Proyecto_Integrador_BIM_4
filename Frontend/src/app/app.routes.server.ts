0e44d75d5a51c1e8ab7c9e9bafc5a9e1be17d4a4
andersonmucia-2025337 <andersonmucia-2025337@kinal.edu.gt>
2026-09-16 00:55:38 -0600

    feat: se agregó la configuración de renderizado del lado del cliente en serverRoutes

--- Frontend/src/app/app.routes.server.ts
+++ Frontend/src/app/app.routes.server.ts
@@ -1,8 +1,17 @@
+// src/app/app.routes.server.ts
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    {
+        path: 'login',
+        renderMode: RenderMode.Client
+    },
+    {
+        path: 'register',
+        renderMode: RenderMode.Client
+    },
+    {
+        path: '**',
+        renderMode: RenderMode.Client
+    }
 ];
