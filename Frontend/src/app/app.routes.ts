164d4aaeb440aeeb254b287b7f34bb3ed6a4b98e
andersonmucia-2025337 <andersonmucia-2025337@kinal.edu.gt>
2026-09-16 00:56:26 -0600

    feat: se agregó la redirección predeterminada y el acoplamiento de coreRoutes

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import { coreRoutes } from './features/core/core.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'usuarios',
+        pathMatch: 'full'
+    },
+    ...coreRoutes,
+    {
+        path: '**',
+        redirectTo: 'usuarios'
+    }
+];
