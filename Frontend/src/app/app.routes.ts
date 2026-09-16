60e5c43874b3b2cc118a050025f12ae744de077a
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-09-15 20:53:46 -0600

    feat: configura rutas de Alerta

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,26 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { LayoutComponent } from "./features/operaciones/components/layout/layout.component";
 
-export const routes: Routes = [];
+// Define las rutas principales de la aplicación
+export const routes: Routes = [
+  {
+    path: "operaciones",
+    component: LayoutComponent,
+    children: [
+      {
+        // Carga el módulo de operaciones en la ruta base de operaciones
+        path: "",
+        loadChildren: () =>
+          import("./features/operaciones/operaciones.module").then(
+            (m) => m.OperacionesModule
+          )
+      }
+    ]
+  },
+  {
+    // Redirección global a la sección de operaciones
+    path: "",
+    redirectTo: "/operaciones",
+    pathMatch: "full"
+  }
+];
