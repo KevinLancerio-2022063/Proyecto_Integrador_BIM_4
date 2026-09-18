eb8a7cbee0d49a68aa17064d30c67df16a8b2495
KevinLancerio-2022063 <klancerio-2022063@kinal.edu.gt>
2026-09-18 01:28:23 -0600

    feat: Se ha agregado los providers correspondientes para el proyecto

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-import { ApplicationConfig, provideZonelessChangeDetection } from "@angular/core";
-import { provideRouter } from "@angular/router";
-import { provideHttpClient } from "@angular/common/http";
-import { routes } from "./app.routes";
-
-// Configura la aplicación en modo zoneless
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZonelessChangeDetection(),
-    provideRouter(routes),
-    provideHttpClient()
-  ]
-=======
 import {
     ApplicationConfig,
     provideBrowserGlobalErrorListeners,
@@ -38,5 +24,4 @@ export const appConfig: ApplicationConfig = {
             withInterceptors([authInterceptor])
         )
     ]
->>>>>>> develop
 };
