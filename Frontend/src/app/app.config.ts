e65ab57655ff6d44ebcbe0e75dd17dcc7ff76f21
KevinLancerio-2022063 <klancerio-2022063@kinal.edu.gt>
2026-09-19 13:06:52 -0600

    fix: Se ha corregido el contenido del app.config

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { ApplicationConfig, provideZonelessChangeDetection } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { provideHttpClient } from "@angular/common/http";
@@ -11,32 +10,4 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient()
   ]
-=======
-import {
-    ApplicationConfig,
-    provideBrowserGlobalErrorListeners,
-    provideZonelessChangeDetection
-} from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors, withFetch } from '@angular/common/http';
-import {
-    provideClientHydration,
-    withEventReplay
-} from '@angular/platform-browser';
-
-import { routes } from './app.routes';
-import { authInterceptor } from './features/core/interceptors/auth.interceptor';
-
-export const appConfig: ApplicationConfig = {
-    providers: [
-        provideBrowserGlobalErrorListeners(),
-        provideZonelessChangeDetection(),
-        provideRouter(routes),
-        provideClientHydration(withEventReplay()),
-        provideHttpClient(
-            withFetch(),
-            withInterceptors([authInterceptor])
-        )
-    ]
->>>>>>> develop
 };
